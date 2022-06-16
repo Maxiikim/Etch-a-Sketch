fc3b6da1915836476d1c6da918e586e76f93a049
Maxiikim <47909104+Maxiikim@users.noreply.github.com>
2022-06-15 20:34:08 -0700

    saved hover effect into a function; created button that lets user determine sketch box size

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
+const body = document.querySelector('body');
 const container = document.querySelector('.container');
+const button = document.createElement('button');
+
+button.textContent = 'Choose box size';
+body.insertBefore(button, body.firstChild).className = 'button';
+button.addEventListener('click', () => {
+    let input = prompt('How many squares do you want on each side of the box?');
+    if (input > 100){
+        alert('Square is too big! Choose a smalller number!')
+    } else {
+        createGrid(input, input);
+        sketch();
+    }
+});
 
 function createGrid(rows, cols){
     let amount = rows * cols;
     for(i = 0; i < amount; i++){
-        const box = document.createElement('div');
-        container.appendChild(box).className = 'grid-box';
+        const div = document.createElement('div');
+        container.appendChild(div).className = 'grid-box';
+        let size = 960 / rows;
+        div.setAttribute('style', `height: ${size}px; width: ${size}px;`)
     }
 }
 
-createGrid(16, 16)
 
-const colorBoxes = document.querySelectorAll('.grid-box');
-const colorBox = document.querySelector('.grid-box')
-
-colorBoxes.forEach(colorBox => colorBox.addEventListener('mouseover', () =>{
-    colorBox.setAttribute('style', 'background-color: black;');
-}));
-
-// function hover() {
-//     box.addEventListener('mouseover', () => {
-//     console.log('div hovered');
-//     box.setAttribute('style', 'backgroud-coior: black;')
-// })};
+function sketch(){
+    const box = document.querySelectorAll('.grid-box');
+    const square = document.querySelector('.grid-box');
+    box.forEach(square => square.addEventListener('mouseover', () =>{
+        let att = square.getAttribute('style');
+        square.setAttribute('style', `${att} background-color: black;`);
+    }));
+}
